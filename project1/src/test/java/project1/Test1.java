505b236cf77a57057d5d36860ab9b0cc9ca978a9
lenovo <lenovo@Lenovo-PC>
2019-01-08 16:18:22 +0530

    111

---
+++ project1/src/test/java/project1/Test1.java
@@ -0,0 +1,10 @@
+package project1;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void testMethod1() {
+		System.out.println("project 1");
+	}
+}
---
+++ project2/src/test/java/project2/Test2.java
@@ -0,0 +1,10 @@
+package project2;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void testMethod2() {
+		System.out.println("project 2");
+	}
+}
